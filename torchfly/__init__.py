43ee6f04533c9178fe521405862ed2dae1e947e9
qywu <wilwu@ucdavis.edu>
2019-10-03 14:25:31 -0700

    set random seed update

--- torchfly/__init__.py
+++ torchfly/__init__.py
@@ -1,5 +1,6 @@
 from torchfly import transformers
 from torchfly import criterions
+from .utils import set_random_seed
 
 # __all__ = ["__version__"]
 __version__ = '0.0.1'
--- torchfly/utils/__init__.py
+++ torchfly/utils/__init__.py
@@ -1,1 +1,2 @@
 from .progress_bar import progress_bar, master_bar
+from .set_random_seed import set_random_seed
