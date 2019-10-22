f28a5dba4fd3c6ca57d397e30a9e14ba505f4564
qywu <wilwu@ucdavis.edu>
2019-10-21 21:34:59 -0700

    tiny update

--- torchfly/transformers/gpt_model.py
+++ torchfly/transformers/gpt_model.py
@@ -21,31 +21,6 @@ def gelu(x):
         1 + torch.tanh(0.7978845608028654 * (x + 0.044715 * torch.pow(x, 3)))
     )
 
-
-def prune_conv1d_layer(layer, index, dim=1):
-    """ Prune a Conv1D layer (a model parameters) to keep only entries in index.
-        A Conv1D work as a Linear layer (see e.g. BERT) but the weights are transposed.
-        Return the pruned layer as a new layer with requires_grad=True.
-        Used to remove heads.
-    """
-    index = index.to(layer.weight.device)
-    W = layer.weight.index_select(dim, index).clone().detach()
-    if dim == 0:
-        b = layer.bias.clone().detach()
-    else:
-        b = layer.bias[index].clone().detach()
-    new_size = list(layer.weight.size())
-    new_size[dim] = len(index)
-    new_layer = Conv1D(new_size[1], new_size[0]).to(layer.weight.device)
-    new_layer.weight.requires_grad = False
-    new_layer.weight.copy_(W.contiguous())
-    new_layer.weight.requires_grad = True
-    new_layer.bias.requires_grad = False
-    new_layer.bias.copy_(b.contiguous())
-    new_layer.bias.requires_grad = True
-    return new_layer
-
-
 class Conv1D(nn.Module):
     def __init__(self, nf, nx):
         super(Conv1D, self).__init__()
@@ -374,9 +349,9 @@ class GPT2SmallConfig:
     n_embd = 768
     n_layer = 12
     n_head = 12
-    resid_pdrop = 0.1
-    embd_pdrop = 0.1
-    attn_pdrop = 0.1
+    resid_pdrop = 0.0
+    embd_pdrop = 0.0
+    attn_pdrop = 0.0
     layer_norm_epsilon = 1e-5
     initializer_range = 0.02
     gradient_checkpointing = False
