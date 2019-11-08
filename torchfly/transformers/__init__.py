0fad756103b28123072dfeb152351c28ee02b844
xiao00yang <qingxiaoyang.zhu@gmail.com>
2019-11-08 00:51:27 -0800

    Added user models to __init__.py

--- torchfly/transformers/__init__.py
+++ torchfly/transformers/__init__.py
@@ -2,3 +2,4 @@ from .gpt_model import GPT2Model, GPT2SimpleLM
 from .bert_model import BertModel
 from .gpt_tokenizer import GPT2Tokenizer
 from .unified_tokenizer import UnifiedTokenizer
+from .user_model import GPT2SimpleLMwithCopyEncoder, GPT2SimpleLMwithCopyDecoder
--- torchfly/transformers/gpt_model.py
+++ torchfly/transformers/gpt_model.py
@@ -351,6 +351,7 @@ class GPT2SimpleLM(nn.Module):
 
         # Fast way to compute lower triangle attention mask
         # shape: (batch, num_head, key_length, query_length/seq_length)
+        # print("Mask size: ", mask.shape)
         mask = mask.view(input_ids.shape[0], 1, 1, mask.shape[1]).repeat(
             1, self.config.n_head, mask.shape[1], 1
         )
@@ -379,4 +380,158 @@ class GPT2SmallConfig:
     attn_pdrop = 0.1
     layer_norm_epsilon = 1e-5
     initializer_range = 0.02
-    gradient_checkpointing = False
+    gradient_checkpointing = True
+
+
+class GPT2SimpleLMwithCopyEncoder(nn.Module):
+    """OpenAI GPT-2 model with a Language Modeling head ("Language Models are Unsupervised Multitask Learners").
+    """
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+        self.transformer = GPT2Model(config)
+        self.lm_head = GPT2LMHead(self.transformer.wte.weight, config)
+        self.apply(self.init_weights)
+        # todo: use config to set the input and out put dimension of linear transformation
+        # self.linear_copy = nn.Linear(, )
+
+    @classmethod
+    def from_pretrained(cls, modelname):
+        if modelname == "unified-gpt2-small":
+            model = cls(GPT2SmallConfig)
+            url = "https://drive.google.com/uc?id=1C5uuC2RNMwIjLC5UInmoEVXbX-U1OEvF"
+            filepath = gdrive_download(url, "models", "unified-gpt2-small.pth")
+            states_dict = torch.load(filepath)
+            model.load_state_dict(states_dict)
+            return model
+
+    def init_weights(self, module):
+        """ Initialize the weights.
+        """
+        if isinstance(module, (nn.Linear, nn.Embedding)):
+            module.weight.data.normal_(
+                mean=0.0, std=self.config.initializer_range
+            )
+        elif isinstance(module, LayerNorm):
+            module.bias.data.zero_()
+            module.weight.data.fill_(1.0)
+        if isinstance(module, nn.Linear) and module.bias is not None:
+            module.bias.data.zero_()
+
+    def set_tied(self):
+        """ Make sure we are sharing the embeddings
+        """
+        self.lm_head.set_embeddings_weights(self.transformer.wte.weight)
+
+    def forward(self, input_ids, position_ids=None, past=None, mask=None):
+
+        if past is None:
+            past_length = input_ids.shape[1]
+        else:
+            # count self
+            past_length = past[0].shape[3] + input_ids.shape[1]
+
+        if mask is None:
+            # print("mask is not provided")
+            mask = torch.ones(
+                input_ids.shape[0],
+                past_length,
+                dtype=torch.bool,
+                device=input_ids.device
+            )
+
+        # Fast way to compute lower triangle attention mask
+        # shape: (batch, num_head, key_length, query_length/seq_length)
+        mask = mask.view(input_ids.shape[0], 1, 1, mask.shape[1]).repeat(
+            1, self.config.n_head, mask.shape[1], 1
+        )
+        mask = mask & mask.permute(0, 1, 3, 2)
+        mask = torch.tril(mask.byte())
+        mask = mask.bool()
+        mask = mask[:, :, -input_ids.shape[1]:, :]
+
+        hidden_states, presents = self.transformer(
+            input_ids, position_ids, past, mask
+        )
+        # lm_logits = self.lm_head(hidden_states)
+        # probability_copy = torch.sigmoid(linear_copy(hidden_states))
+        # return lm_logits, presents
+        return hidden_states, presents
+
+class GPT2SimpleLMwithCopyDecoder(nn.Module):
+    """OpenAI GPT-2 model with a Language Modeling head ("Language Models are Unsupervised Multitask Learners").
+    """
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+        self.transformer = GPT2Model(config)
+        self.lm_head = GPT2LMHead(self.transformer.wte.weight, config)
+        self.apply(self.init_weights)
+        # todo: use config to set the input and out put dimension of linear transformation
+        # self.linear_copy = nn.Linear(, )
+
+    @classmethod
+    def from_pretrained(cls, modelname):
+        if modelname == "unified-gpt2-small":
+            model = cls(GPT2SmallConfig)
+            url = "https://drive.google.com/uc?id=1C5uuC2RNMwIjLC5UInmoEVXbX-U1OEvF"
+            filepath = gdrive_download(url, "models", "unified-gpt2-small.pth")
+            states_dict = torch.load(filepath)
+            model.load_state_dict(states_dict)
+            return model
+
+    def init_weights(self, module):
+        """ Initialize the weights.
+        """
+        if isinstance(module, (nn.Linear, nn.Embedding)):
+            module.weight.data.normal_(
+                mean=0.0, std=self.config.initializer_range
+            )
+        elif isinstance(module, LayerNorm):
+            module.bias.data.zero_()
+            module.weight.data.fill_(1.0)
+        if isinstance(module, nn.Linear) and module.bias is not None:
+            module.bias.data.zero_()
+
+    def set_tied(self):
+        """ Make sure we are sharing the embeddings
+        """
+        self.lm_head.set_embeddings_weights(self.transformer.wte.weight)
+
+    def forward(self, input_ids, position_ids=None, past=None, mask=None):
+
+        if past is None:
+            past_length = input_ids.shape[1]
+        else:
+            # count self
+            past_length = past[0].shape[3] + input_ids.shape[1]
+
+        if mask is None:
+            # print("mask is not provided")
+            mask = torch.ones(
+                input_ids.shape[0],
+                past_length,
+                dtype=torch.bool,
+                device=input_ids.device
+            )
+
+        # Fast way to compute lower triangle attention mask
+        # shape: (batch, num_head, key_length, query_length/seq_length)
+        mask = mask.view(input_ids.shape[0], 1, 1, mask.shape[1]).repeat(
+            1, self.config.n_head, mask.shape[1], 1
+        )
+        mask = mask & mask.permute(0, 1, 3, 2)
+        mask = torch.tril(mask.byte())
+        mask = mask.bool()
+        mask = mask[:, :, -input_ids.shape[1]:, :]
+
+        hidden_states, presents = self.transformer(
+            input_ids, position_ids, past, mask
+        )
+        lm_logits = self.lm_head(hidden_states)
+        # get last hidden_states
+        # copy_prob = torch.sigmoid(linear_copy(hidden_states))
+
+        # update logits as the fusion of generation distribution and copy distribution
+        
+        return lm_logits, presents
