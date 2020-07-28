d761d3b0d0a97338d3374cf811afbf5edd48c98f
Qingyang Wu <wilwu@ucdavis.edu>
2020-07-28 01:24:15 -0700

    update get_rank

--- torchfly/common/__init__.py
+++ torchfly/common/__init__.py
@@ -3,3 +3,4 @@ from .move_to_device import move_to_device
 from .logging_util import configure_logging
 from .random_seeding import set_random_seed
 from .launch_distributed import launch_distributed
+from .get_rank import get_rank
--- torchfly/training/trainer_loop.py
+++ torchfly/training/trainer_loop.py
@@ -13,7 +13,7 @@ from apex.parallel import DistributedDataParallel, Reducer
 # local imports
 from torchfly.training.callbacks import Callback, CallbackHandler, Events
 from torchfly.training.callbacks import LogHandler, GradientClipNorm, Checkpoint
-from torchfly.common import move_to_device
+from torchfly.common import move_to_device, get_rank
 from torchfly.training import FlyModel
 
 import logging
@@ -21,25 +21,6 @@ import logging
 logger = logging.getLogger(__name__)
 
 # pylint: disable=no-member
-
-
-def get_rank():
-    """
-    We use environment variables to pass the rank info
-    Returns:
-        rank: rank in the multi-node system 
-        local_rank: local rank on a node
-    """
-    if "RANK" not in os.environ or "LOCAL_RANK" not in os.environ:
-        rank = 0
-        local_rank = 0
-        os.environ["RANK"] = str(0)
-        os.environ["LOCAL_RANK"] = str(0)
-    else:
-        rank = int(os.environ["RANK"])
-        local_rank = int(os.environ["LOCAL_RANK"])
-
-    return rank, local_rank
 
 
 class TrainerLoop:
